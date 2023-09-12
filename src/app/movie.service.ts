1cb05c3c614ffaf5d7c72286e0e1a6529ff2ebd9
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 13:17:45 +0300

    Http requests create

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -3,21 +3,26 @@ import {Movie} from './movie';
 import {Movies} from './movie.datasource';
 import { Observable, of } from 'rxjs';
 import { LoggingService } from './logging.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  constructor(private loggingService: LoggingService) { }
+  private apiMoviesUrl = 'api/movies';
+  constructor(
+    private loggingService: LoggingService,
+    private  http: HttpClient
+    ) { }
 
   getMovies(): Observable<Movie[]> {
     this.loggingService.add('MovieService: lsiting movies');
-    return of (Movies);
+    return this.http.get<Movie[]>(this.apiMoviesUrl);
   }
 
   getMovie(id): Observable<Movie> {
     this.loggingService.add('MovieService: get detail by id='+id)
-    return of(Movies.find(movie => movie.id === id));
+    return this.http.get<Movie>(this.apiMoviesUrl+'/'+id);
   }
 
 }
